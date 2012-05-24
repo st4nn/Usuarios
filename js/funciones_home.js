98c009640a69367a1ca0fb6fc54a363a6f0a8c3a
Stan <jhonathan2@gmial.com>
2012-05-24 12:09:03 -0500

    a

---
+++ js/funciones_home.js
@@ -0,0 +1,94 @@
+﻿var Usuario;
+$(document).on("ready", arranque);
+
+function arranque()
+{
+	if(!localStorage.Usuario)
+	{CerrarSesion();}
+	
+	$("#cboLanguage").on("change", CambiarIdioma);
+	$("#cboToolsType").on("change", CambiarTipoCustomPlayer);
+	
+	$("txtMyAccount_Company").on("change", txtMyAccount_Company_Leave)
+	
+	$("#lnkLogout").on("click", CerrarSesion);
+	
+	//$("#tabs").tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
+	//$("#tabs li").removeClass('ui-corner-top').addClass('ui-corner-left');
+	
+	$("#tabs").tabs();
+	$("#divTools").tabs();	
+	$("#MyAccount_Options").tabs();	
+		
+	$("#cboLanguage").load('php/CargarIdiomas.php');
+	CargarUsuario();
+}
+function CambiarIdioma()
+{
+		$.post("php/CambiarIdioma.php",  
+		{
+			Idioma: $("#cboLanguage").val()
+		}, 
+		function(data)
+		{	
+			$("#lblWelcome").text(data.Welcome + " " + Usuario.Name);
+			$("#lnkHome").text(data.Home);
+			$("#lnkTools").text(data.Tools);
+			$("#lnkAnalytics").text(data.Analytics);
+			$("#lnkMyAccount").text(data.MyAccount);
+			$("#lnkLogout").text(data.Logout);
+			
+			$("#lnkCustomPlayer").text(data.CustomPlayer);
+			$("#lnkExportCode").text(data.ExportCode);
+			
+			$("#lnkPersonalInformation").text(data.PersonalInformation);
+			$("#lnkCustomTemplate").text(data.CustomTemplate);
+			
+			$("#lblName").text(data.Name);
+			$("#lblDisplayName").text(data.DisplayName);
+			$("#lblEmail").text(data.Email);
+			$("#lblCompany").text(data.Company);
+			$("#btnMyAccount_Save").val(data.Save);
+		}, 
+		"json");	
+}
+function CambiarTipoCustomPlayer()
+{
+	if ($("#cboToolsType").val())
+	{
+		$("#IframeCustomPlayer").load($("#cboToolsType").val());	
+	}
+}
+function CargarUsuario()
+{
+	Usuario = JSON.parse(localStorage.Usuario)[0];
+	$("#lblWelcome span").text(Usuario.Name);
+	
+	$("#txtMyAccount_Name").val(Usuario.Name);
+	$("#txtMyAccount_DisplayName").val(Usuario.NickName);
+	$("#txtMyAccount_Email").val(Usuario.Email);
+	$("#txtMyAccount_Company").val(Usuario.CompanyName);
+	$("#txtMyAccount_Facebook").val(Usuario.urlFacebook);
+	$("#txtMyAccount_Twitter").val(Usuario.urlTwitter);
+}
+function CerrarSesion()
+{
+	delete localStorage.Usuario;
+	window.location.replace("index.html");
+}
+function txtMyAccount_Company_Leave()
+{
+	alert("mao");
+/*$.post("php/VerificarCompania.php",  
+		{
+			Name: $("#txtMyAccount_Company").val()
+		}, 
+		function(data)
+		{	
+			if (!data)
+			{alert("nada");}
+			else{
+				alert("Si hubo");
+				}
+		});		*/
+}
